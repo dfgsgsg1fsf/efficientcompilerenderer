5e0d4d6842d9d7de2ee5f68900b61118bcbe3017
dfgsgsg1fsf <js1katohi12@hotmail.com>
2024-05-08 15:15:45 +0700

    feat

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-function mergeSort(arr) {
-  if (arr.length <= 1) return arr;
+const mergeSort = (arr) => {
+  if (arr.length <= 1) {
+    return arr;
+  }
   const mid = Math.floor(arr.length / 2);
   const left = mergeSort(arr.slice(0, mid));
   const right = mergeSort(arr.slice(mid));
   return merge(left, right);
-}
-function merge(left, right) {
+};
+const merge = (left, right) => {
   let result = [];
   let leftIndex = 0;
   let rightIndex = 0;
@@ -19,4 +21,4 @@ function merge(left, right) {
     }
   }
   return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
-}
+};
